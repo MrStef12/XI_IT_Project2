55dbba118e5e7409b7ba3695ac57520702cefcb4
Steffen Hansen <stef.hansen@live.dk>
2017-04-25 16:42:24 +0200

    Initial commit

---
+++ XI_IT_Project2/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XI_IT_Project2.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
