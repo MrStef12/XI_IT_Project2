730afcb2516a5a77bcdafbff6d1efbd58f277336
Steffen Hansen <stef.hansen@live.dk>
2017-05-03 09:49:55 +0200

    Work on Image model and New view

--- XI_IT_Project/Models/Image.cs
+++ XI_IT_Project/Models/Image.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,24 @@ namespace XI_IT_Project_assignment_2.Data.Models
     public class Image
     {
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Image title")]
         public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail address")]
         public string Email { get; set; }
-        public string ImgUrl {get;set;}
+
+        [Required]
+        [Url]
+        [Display(Name = "Image URL")]
+        public string ImgUrl { get; set; }
     }
 }
