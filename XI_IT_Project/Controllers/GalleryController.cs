bd817e6c792e0750b7e349f357dead0bbaffe39c
Ditlev <ditlev.stjerne@gmail.com>
2017-05-03 07:35:04 +0200

    Changes and shit

--- XI_IT_Project/Controllers/GalleryController.cs
+++ XI_IT_Project/Controllers/GalleryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using XI_IT_Project_assignment_2.Data;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +13,17 @@ namespace XI_IT_Project.Controllers
 {
     public class GalleryController : Controller
     {
-        // GET: /Gallery/
-        public IActionResult Index()
+        private DataContext _context;
+
+        public GalleryController(DataContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // GET: /Gallery/
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Images.ToListAsync());
         }
     }
 }
